575887848746e8a14e519c7d9a5410399dfdcd1c
yessinedev <yessine.ag@gmail.com>
2024-11-26 12:48:44 +0100

    update navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/clerk-react";
-import { Film, Search, Bell, UserCircle } from "lucide-react";
+import { Film, UsersRound, Award, LogOut, User2 } from "lucide-react";
+import { Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Navbar() {
   const { isSignedIn, user } = useUser();
 
   const getUserIdentifier = () => {
     if (user?.firstName) {
-      return user.firstName;
+      const fullName = `${user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1)} ${user?.lastName.charAt(0).toUpperCase() + user.lastName.slice(1)}`
+      return fullName;
     }
     return user?.primaryEmailAddress?.emailAddress || "User";
   };
@@ -14,33 +24,52 @@ export default function Navbar() {
     <nav className="fixed top-0 z-50 w-full border-b border-gray-800 bg-black/95 backdrop-blur-sm">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center space-x-3">
+          <Link to="/" className="flex items-center space-x-3">
             <Film className="w-8 h-8 text-purple-500" />
             <span className="text-xl font-bold text-transparent bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text">
               CineClub
             </span>
-          </div>
+          </Link>
 
-          <div className="items-center flex-1 hidden max-w-xl mx-8 md:flex">
-            <div className="relative w-full">
-              <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Search clubs, movies, or shows..."
-                className="w-full py-2 pl-10 pr-4 text-gray-300 bg-gray-900 border border-gray-700 rounded-full focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
-              />
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2 border px-3 py-2 border-gray-800 rounded-xl hover:border-purple-500/50">
+              <UsersRound size={18} />
+              <Link to="/clubs">Clubs</Link>
+            </div>
+            <div className="flex items-center gap-2 border px-3 py-2 border-gray-800 rounded-xl hover:border-purple-500/50">
+              <Award size={18} />
+              <Link to="/leaderboard">LeaderBoard</Link>
             </div>
           </div>
           <div>
             {isSignedIn ? (
-              <div className="flex items-center gap-4">
-                <SignOutButton />
+              <div className="flex items-center gap-4 px-3 py-2 border border-gray-800 rounded-xl hover:border-purple-500/50">
                 <span>Welcome, {getUserIdentifier()}</span>
-                <img
-                  src={user.imageUrl}
-                  alt={getUserIdentifier()}
-                  className="w-8 h-8 rounded-full"
-                />
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="outline-none">
+                    <img
+                      src={user.imageUrl}
+                      alt={getUserIdentifier()}
+                      className="w-8 h-8 rounded-full"
+                    />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                    <Link to="/profile" className="flex items-center justify-center space-x-2 bg-black/20 hover:bg-black/40 text-white rounded-lg backdrop-blur-sm transition-colors">
+                        <User2 className="w-2 h-2" />
+                        <span>Profile</span>
+                      </Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                      <button className="flex items-center justify-center space-x-2 bg-black/20 hover:bg-black/40 text-white rounded-lg backdrop-blur-sm transition-colors">
+                        <LogOut className="w-2 h-2" />
+                        <SignOutButton />
+                      </button>
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
             ) : (
               <SignInButton mode="modal" />
---
+++ client/src/pages/LeaderBoardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LeaderBoardPage = () => {
+  return (
+    <div className='py-32'>LeaderBoardPage</div>
+  )
+}
+
+export default LeaderBoardPage
