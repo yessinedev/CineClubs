e0292f9ac30eb5034d4397314566b915eedad251
Rami Chabchoub <ramichabchoub.rc@gmail.com>
2024-11-26 17:17:47 +0100

    refactor: api client setup (#34)

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -1,30 +1,34 @@
-import axios from "axios"
+import apiClient from "./apiClient";
 
 export const likePost = async (postId, userId) => {
-    return await axios.post(`http://localhost:8080/api/posts/${postId}/like?userId=${userId}`)
-}
+  return await apiClient.post(`/posts/${postId}/like?userId=${userId}`);
+};
 
 export const unlikePost = async (postId, userId) => {
-    return await axios.post(`http://localhost:8080/api/posts/${postId}/unlike?userId=${userId}`)
-}
+  return await apiClient.post(`/posts/${postId}/unlike?userId=${userId}`);
+};
 
-export const fetchClubPosts = async(clubId, userId) => {
-    const {data} = await axios.get(`http://localhost:8080/api/posts/club/${clubId}?userId=${userId}`)
-    return data;
-}
+export const fetchClubPosts = async (clubId, userId) => {
+  const { data } = await apiClient.get(
+    `/posts/club/${clubId}?userId=${userId}`
+  );
+  return data;
+};
 
 export const createPost = async (post, userId, clubId) => {
-    try {
-      console.log("Creating post:", post);
-      const { data } = await axios.post(
-        `http://localhost:8080/api/posts?userId=${userId}&clubId=${clubId}`,
-        { title: post.title, content: post.content }
-      );
-      console.log("Post created successfully:", data);
-      return data;
-    } catch (error) {
-      console.error("Error creating post:", error.response?.data || error.message);
-      throw error;
-    }
-  };
-  
+  try {
+    console.log("Creating post:", post);
+    const { data } = await apiClient.post(
+      `/posts?userId=${userId}&clubId=${clubId}`,
+      { title: post.title, content: post.content }
+    );
+    console.log("Post created successfully:", data);
+    return data;
+  } catch (error) {
+    console.error(
+      "Error creating post:",
+      error.response?.data || error.message
+    );
+    throw error;
+  }
+};
